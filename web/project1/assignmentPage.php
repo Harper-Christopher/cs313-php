3b170af9d9df31198d14c8c9800a6876f8a10255
Harper-Christopher <christopherharper123@gmail.com>
2021-01-15 21:12:23 -0700

    02 prove assignment

---
+++ web/project1/assignmentPage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>CSE 341 | Christopher Harper | BYU-Idaho </title>
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/middle.css">
+  <link rel="stylesheet" href="css/small.css">
+</head>
+
+<body>
+
+  <main>
+    <div class="center">
+    <img id="construction" src="img/underConstruction.jpg" alt="Under Construction Photo">
+    </div>
+    <p class="comingSoon">Coming Soon!</p>
+  </main>
+
+</body>
+
+</html>
---
+++ web/project1/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>CSE 341 | Christopher Harper | BYU-Idaho </title>
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/middle.css">
+  <link rel="stylesheet" href="css/small.css">
+</head>
+
+<body>
+  <header>
+    <img id="photo" src="img/christopher.jpg" alt="Christopher Harper's photo">
+    <h1>Christopher Harper</h1>
+
+  </header>
+
+  <main>
+    <h2>Introduction Page</h2>
+    <hr>
+
+    <p>Hey everyone!</p>
+    <p>My name is Christopher Harper and on this website we will be going through projects and assignments from BYU-I's CSE 341 class. Click the link below to begin exploring all the projects and fun this course has to offer over the next 14 weeks.</p>
+
+    <ul>
+      <li>Assignments Page: <a href="assignmentPage.php">Click to view the assignments page.</a></li>
+    </ul>
+
+    <div class="date"><p>Current Date: <?php echo date('m/d/Y') ?></p></div>
+
+  </main>
+
+  <footer>
+  </footer>
+
+</body>
+
+</html>

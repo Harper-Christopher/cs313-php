b0b6b91e20be73d4af784c70560c615bf584541f
Harper-Christopher <christopherharper123@gmail.com>
2021-02-03 21:13:23 -0700

    update

--- web/project1/browse.php
+++ web/project1/browse.php
@@ -46,54 +46,54 @@ if (isset($_POST['price6'])) {
 }
 
 
-try {
-  // default Heroku Postgres configuration URL
-  // this is a built in function in php to get the value from an enviornment variable
-  $dbUrl = getenv('DATABASE_URL');
+// try {
+//   // default Heroku Postgres configuration URL
+//   // this is a built in function in php to get the value from an enviornment variable
+//   $dbUrl = getenv('DATABASE_URL');
 
-  //if we are on heroku this will be set otherwise we can check for a local connection
-  //heroku takes care of all of this for us
-  if (!isset($dbUrl) || empty($dbUrl)) {
-      // example localhost configuration URL with 
-      // user: "my_username"
-      // password: "my_password"
-      // database: "my_database"
+//   //if we are on heroku this will be set otherwise we can check for a local connection
+//   //heroku takes care of all of this for us
+//   if (!isset($dbUrl) || empty($dbUrl)) {
+//       // example localhost configuration URL with 
+//       // user: "my_username"
+//       // password: "my_password"
+//       // database: "my_database"
 
-      // hardcoded for debugging only not for production site
-      $dbUrl = "postgres://my_username:my_password@localhost:5432/my_database";
-  }
+//       // hardcoded for debugging only not for production site
+//       $dbUrl = "postgres://my_username:my_password@localhost:5432/my_database";
+//   }
 
-  // Get the various parts of the DB Connection from the URL
-  $dbopts = parse_url($dbUrl);
+//   // Get the various parts of the DB Connection from the URL
+//   $dbopts = parse_url($dbUrl);
 
-  $dbHost = $dbopts["host"];
-  $dbPort = $dbopts["port"];
-  $dbUser = $dbopts["user"];
-  $dbPassword = $dbopts["pass"];
-  $dbName = ltrim($dbopts["path"],'/');
+//   $dbHost = $dbopts["host"];
+//   $dbPort = $dbopts["port"];
+//   $dbUser = $dbopts["user"];
+//   $dbPassword = $dbopts["pass"];
+//   $dbName = ltrim($dbopts["path"],'/');
 
-  // Create the PDO connection
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+//   // Create the PDO connection
+//   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-  // this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
-  $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-  //Now we can use $db->
-  $statement = $db->prepare('SELECT guitarid, guitarname, price FROM guitar');
-  $statement->execute();
+//   // this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+//   $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+//   //Now we can use $db->
+//   $statement = $db->prepare('SELECT guitarid, guitarname, price FROM guitar');
+//   $statement->execute();
 
-  // Go through each result
-  while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-  {
-      echo $row['id'];
-  }
-}
-catch (PDOException $ex) {
-  // for debugging only not for production site
-  echo "Error connecting to DB. Details: $ex";
-  die();
-}
+//   // Go through each result
+//   while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+//   {
+//       echo $row['id'];
+//   }
+// }
+// catch (PDOException $ex) {
+//   // for debugging only not for production site
+//   echo "Error connecting to DB. Details: $ex";
+//   die();
+// }
 
-return $db;
+// return $db;
 
 
 ?><!DOCTYPE html>
@@ -116,7 +116,7 @@ return $db;
   <a href="cart.php">View Shopping Cart</a>
   </div>
 
-  <h1><?php echo $db['guitarname'];?></h1>
+  <!-- <h1><?php echo $db['guitarname'];?></h1> -->
 
   <div class="flex">
     <form action="browse.php" method="post">

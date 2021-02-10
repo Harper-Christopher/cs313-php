ae7e8bcb37110efbab504c4786153da3ecf4fe04
Harper-Christopher <christopherharper123@gmail.com>
2021-02-09 22:26:02 -0700

    update

--- web/project1/register.php
+++ web/project1/register.php
@@ -21,11 +21,9 @@ $db = db_connect();
           setcookie('firstname', $userfirstname, strtotime('+1 year'), '/');
           $_SESSION['message'] = "Thanks for registering $userfirstname. Please use your email and password to login.";
           header('Location: login.php');
-          exit;
       } else {
           $message = "<p class='formErrorMessage'>Sorry $userfirstname, but the registration failed. Please try again.</p>";
           header('Location: register.php');
-          exit;
       }
 
 //Function to check the value of the $useremail variable, after having been sanitized, to see if it "looks" like a valid email address.

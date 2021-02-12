7b85bf90ee2e61f58a535d05fa286a6733c1d122
Harper-Christopher <christopherharper123@gmail.com>
2021-02-11 21:42:03 -0700

    update

--- web/project1/accounts.php
+++ web/project1/accounts.php
@@ -1,10 +1,9 @@
 <?php
 session_start();
 
-$action = filter_input(INPUT_GET, 'action');
-if ($action == NULL) {
+
     $action = filter_input(INPUT_POST, 'action');
-}
+
 
 
 // Get the database connection file

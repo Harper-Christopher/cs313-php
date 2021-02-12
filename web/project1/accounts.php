030eadf339c3694a849c140fc5bbd6634a4ff220
Harper-Christopher <christopherharper123@gmail.com>
2021-02-11 22:12:03 -0700

    update

--- web/project1/accounts.php
+++ web/project1/accounts.php
@@ -1,13 +1,51 @@
 <?php
 
+require_once 'model.php';
+require_once 'connection.php';
+$db = db_connect();
+
 $userfirstname = htmlspecialchars($_POST['userfirstname']);
 $userlastname = htmlspecialchars($_POST['userlastname']);
 $useremail = htmlspecialchars($_POST['useremail']);
 $userpassword = htmlspecialchars($_POST['userpassword']);
 
-echo "$userfirstname\n";
-echo "$userlastname\n";
-echo "$useremail\n";
-echo "$userpassword\n";
 
+
+        $useremail = checkEmail($useremail);
+        $checkassword = checkPassword($userpassword);
+
+        $existingEmail = checkExistingEmail($useremail);
+
+        // Check for existing email address in the table
+        if ($existingEmail) {
+            $message = '<p class="formErrorMessage">Email address entered already exists. Please use your email to login.</p>';
+            include 'login.php';
+            die();
+        }
+
+        // Check for missing data
+        if (empty($userfirstname) || empty($userlastname) || empty($useremail) || empty($userpassword)) {
+            $message = '<p class="formErrorMessage">Please provide information for all empty form fields.</p>';
+            include 'register.php';
+            die();
+        }
+
+
+        // Hash the checked password
+        $hashedPassword = password_hash($userpassword, PASSWORD_DEFAULT);
+
+        // Send the data to the model
+        $regOutcome = regUser($userfirstname, $userlastname, $useremail, $hashedPassword);
+
+        // Check and report the result
+        if ($regOutcome === 1) {
+            setcookie('firstname', $userfirstname, strtotime('+1 year'), '/');
+            $_SESSION['message'] = "Thanks for registering $userfirstname. Please use your email and password to login.";
+            header('Location: login.php');
+            die();
+        } else {
+            $message = "<p class='formErrorMessage'>Sorry $userfirstname, but the registration failed. Please try again.</p>";
+            include 'register.php';
+            die();
+        }
 ?>

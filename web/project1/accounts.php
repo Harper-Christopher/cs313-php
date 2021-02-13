87afdcef9e4c33043d2a0fe5e46800e9dfce8012
Harper-Christopher <christopherharper123@gmail.com>
2021-02-12 20:03:51 -0700

    update

--- web/project1/accounts.php
+++ web/project1/accounts.php
@@ -1,15 +1,14 @@
 <?php
 
-require_once 'model.php';
-require_once 'connection.php';
-$db = db_connect();
-
 $userfirstname = htmlspecialchars($_POST['userfirstname']);
 $userlastname = htmlspecialchars($_POST['userlastname']);
 $useremail = htmlspecialchars($_POST['useremail']);
 $userpassword = htmlspecialchars($_POST['userpassword']);
 
-$stmt = $db->prepare('INSERT INTO users (userfirstname, userlastname, useremail, userpassword)
+require_once 'connection.php';
+$db = db_connect();
+
+$stmt = $db->prepare('INSERT INTO users(userfirstname, userlastname, useremail, userpassword)
 VALUES (:userfirstname, :userlastname, :useremail, :userpassword);');
 $stmt->bindValue(':userfirstname', $userfirstname, PDO::PARAM_STR);
 $stmt->bindValue(':userlastname', $userlastname, PDO::PARAM_STR);
@@ -17,7 +16,7 @@ $stmt->bindValue(':useremail', $useremail, PDO::PARAM_STR);
 $stmt->bindValue(':userpassword', $userpassword, PDO::PARAM_STR);
 $stmt->execute();
 
-$page = 'login.php';
+$page = "login.php";
 
 header("Location: $page");
 die();

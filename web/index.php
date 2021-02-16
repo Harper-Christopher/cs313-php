289825aac9f8f873552cf7dfc1ffbd1d941d28dd
Harper-Christopher <christopherharper123@gmail.com>
2021-02-15 19:54:47 -0700

    update

--- web/index.php
+++ web/index.php
@@ -25,7 +25,7 @@
     <p>My name is Christopher Harper and on this website we will be going through projects and assignments from BYU-I's CSE 341 class. Click the link below to begin exploring all the projects and fun this course has to offer over the next 14 weeks.</p>
 
     <ul>
-      <li>Assignments Page: <a href="assignmentPage.php">Click to view the assignments page.</a></li>
+      <li>Assignments Page: <a href="project1/browse.php">Click to view the assignments page.</a></li>
     </ul>
 
     <div class="date"><p>Current Date: <?php echo date('m/d/Y') ?></p></div>
